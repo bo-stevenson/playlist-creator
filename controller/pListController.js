c261077c03d5d2232e7fd852a76de64936adfea9
Jake <jakekippes123gmail.com>
2021-01-19 20:10:00 -0600

    controller.js work

---
+++ controller/pListController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const playlist = require("../models/playlists.js");
+
+router.get("/", (_req, res) => {
+  playlist.all(data => {
+    const playlistObject = {
+      playlists: data
+    };
+    console.log(playlistObject);
+    res.render("index", playlistObject);
+  });
+});
