d5814dd4f81de6f61a912e0d7ccb81687937b934
Jake <jakekippes123gmail.com>
2021-01-21 21:17:20 -0600

    working on api and sequelize

--- models/playlist.js
+++ models/playlist.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 module.exports = function(sequelize, DataType) {
-  const Playlist = sequelize.define(
-    "playlist",
+  const CreatedPlaylist = sequelize.define(
+    "playlists",
     {
       artist: {
         type: DataType.STRING,
@@ -23,5 +23,89 @@ module.exports = function(sequelize, DataType) {
     { freezeTableName: true }
   );
 
+  return CreatedPlaylist;
+};
+
+module.exports = function(sequelize, DataType) {
+  const Playlist = sequelize.define(
+    "playlists",
+    {
+      party: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      workout: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      chill: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      video_game: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      cleaning: {
+        type: DataType.STRING,
+        allowNull: false
+      }
+    },
+    { freezeTableName: true }
+  );
+
   return Playlist;
 };
+
+module.exports = function(sequelize, DataType) {
+  const Song = sequelize.define(
+    "Songs",
+    {
+      artist: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      song_name: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      song_cover: {
+        type: DataType.STRING,
+        allowNull: false
+      },
+      album_cover: {
+        type: DataType.STRING,
+        allowNull: false
+      }
+    },
+    { freezeTableName: true }
+  );
+  return Song;
+};
+
+module.exports = function(sequelize, DataType) {
+  const PlaylistSongs = sequelize.define(
+    "playlist songs",
+    {
+      PlaylistId: {
+        type: DataType.INTEGER,
+        references: {
+          model: Playlist,
+          key: "id"
+        }
+      },
+      SongId: {
+        type: DataType.INTEGER,
+        references: {
+          model: Song,
+          key: "id"
+        }
+      }
+    },
+    { freezeTableName: true }
+  );
+  return PlaylistSongs;
+};
+
+Playlist.belongsToMany(Song, { through: PlaylistSongs });
+Song.belongsToMany(Playlist, { through: PlaylistSongs });
