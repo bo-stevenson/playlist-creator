5de5a52f537983730417a625ac3bee20971975c8
Christian Goldman <christian.goldman9638@gmail.com>
2021-01-24 20:39:53 -0600

    fixed middleware for css

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("/css"));
+app.use("/assets", express.static("assets"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
