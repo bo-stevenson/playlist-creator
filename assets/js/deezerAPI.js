6cbb5a0026f3ceb176be13ed170076ff10982aa8
Jake <jakekippes123gmail.com>
2021-01-25 17:15:21 -0600

    got api working

--- assets/js/deezerAPI.js
+++ assets/js/deezerAPI.js
@@ -1,25 +1,19 @@
 /* eslint-disable prefer-const */
-
 let song = "";
 let icon = $("#albumIcon");
 let songTitle = $("#songTitle");
 let albumName = $("#albumName");
 let artist = $("#artistName");
 let songSearch = $("#songSearch");
-
 function searchSong(song) {
-  let queryURL = "https://api.deezer.com/search?&jsoncallback=?&q=" + song;
+  const queryURL =
+    "https://cors-anywhere.herokuapp.com/" +
+    "https://api.deezer.com/search/track?q=" +
+    song;
   console.log(queryURL);
   $.ajax({
     url: queryURL,
-    dataType: "jsonp",
-    method: "GET",
-    contentType: "application/json",
-    crossDomain: true,
-    headers: {
-      accept: "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
+    method: "GET"
   }).then((response) => {
     console.log("API call made");
     console.log(response);
@@ -30,7 +24,6 @@ function searchSong(song) {
   });
 }
 console.log("hello");
-
 $("#songSearchBtn").on("click", (event) => {
   event.preventDefault();
   if (songSearch.val().trim() !== "") {
