19cfb55298f01a5b89f029497444b0c3b9a4af9a
Bo Stevenson <bostevenson1027@gmail.com>
2021-01-26 18:30:18 -0600

    another possible fix

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(process.env[config.use_env_variable], config);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
